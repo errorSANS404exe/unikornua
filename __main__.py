f6d107ef76bcafa309b9ef868a2614dbb5602c8f
user <v.torubarova.v@gmail.com>
2021-01-22 12:51:04 +0200

    axmed narisovat pushka

--- __main__.py
+++ __main__.py
@@ -1,4 +1,4 @@
-import pygame, time
+import pygame, time, pomosh
 from pygame import display, event
 
 
@@ -7,18 +7,26 @@ def event_processing():
     for cat in log:
         if cat.type == pygame.QUIT:
             exit()
-
-
-k =pygame.image.load("kartinki/fon.jpg")
+        if cat.type==pygame.KEYDOWN:
+            if cat.key==pygame.K_RIGHT:
+                print("vpravo")
+            if cat.key==pygame.K_LEFT:
+                print("vlevo")
 
 
 def drawing():
-    window.blit(k,[0,0])
+    window.blit(k, [0, 0])
+    window.blit(pushka, [200,400])
     display.flip()
 
 
 pygame.init()
 window = display.set_mode([500, 500])
+
+k = pygame.image.load("kartinki/fon.jpg")
+pushka = pygame.image.load("kartinki/pushka dla programmi axmeda.png")
+pushka = pomosh.izmeni_kartinku(pushka, 100, 100, [255, 255, 255], 50)
+
 while 1 == 1:
     event_processing()
     drawing()
