27634fcf01e7de32698051c5c2fe43efb31ee86b
user <v.torubarova.v@gmail.com>
2021-01-25 20:16:06 +0200

    axmed narisovat pushka

--- __main__.py
+++ __main__.py
@@ -3,25 +3,31 @@ from pygame import display, event
 
 
 def event_processing():
+    global pushkax
     log = event.get()
     for cat in log:
         if cat.type == pygame.QUIT:
             exit()
-        if cat.type==pygame.KEYDOWN:
-            if cat.key==pygame.K_RIGHT:
-                print("vpravo")
-            if cat.key==pygame.K_LEFT:
-                print("vlevo")
+        if cat.type == pygame.KEYDOWN:
+            if cat.key == pygame.K_RIGHT:
+                pushkax += 30
+                if pushkax >= 400:
+                    pushkax = 400
+            if cat.key == pygame.K_LEFT:
+                pushkax -= 30
+                if pushkax <= 0:
+                    pushkax = 0
 
 
 def drawing():
     window.blit(k, [0, 0])
-    window.blit(pushka, [200,400])
+    window.blit(pushka, [pushkax, 400])
     display.flip()
 
 
 pygame.init()
 window = display.set_mode([500, 500])
+pushkax = 200
 
 k = pygame.image.load("kartinki/fon.jpg")
 pushka = pygame.image.load("kartinki/pushka dla programmi axmeda.png")
